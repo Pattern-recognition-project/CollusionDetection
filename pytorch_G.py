802abc9756fdc86758729005aea71ac629a398b8
giadadalcol <114097176+giadadalcol@users.noreply.github.com>
2023-01-19 19:34:13 +0100

    pytorch model

---
+++ pytorch_G.py
@@ -0,0 +1,154 @@
+from data import Data
+import pickle
+
+import numpy as np
+np.random.seed(1773)
+
+import matplotlib.pyplot as plt
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, recall_score, precision_score
+from sklearn.model_selection import train_test_split
+
+from torch_model_G import Net
+
+
+from math import sqrt
+
+
+with open("DB_Collusion_All_processed.obj","rb") as filehandler:
+    data = pickle.load(filehandler)
+
+
+# get train and test data and convert in the correct form
+inputTrain = data.get_train_X()
+targetTrain = data.get_train_y()
+inputTest = data.get_test_X()
+targetTest = data.get_test_y()
+inputTrain = [[[value] for value in auction] for auction in inputTrain]
+inputTest = [[[value] for value in auction] for auction in inputTest]
+
+
+# consider features to be added for the second network
+added_features = data.load_aggegrated()
+added_featuresTrain = added_features[:len(inputTrain)]
+
+# instantiate the model
+model = Net()
+
+allIndices = np.arange(len(targetTrain))
+
+# define the loss function
+# lossFunc = nn.BCELoss() #if this is used the sigmoid layer should be added in the second network
+lossFunc = nn.BCEWithLogitsLoss()
+
+minibatchSize = 32
+
+# number of training epochs
+numEpochs = 10
+
+optimizer = optim.Adam(model.parameters(), lr = 0.0001)
+
+trainLosses = []; testLosses = []
+
+# variable for target values of test set
+testTargetVar = Variable(torch.from_numpy(np.stack(targetTest)))
+
+
+
+
+print("starting training")
+for epoch in range(numEpochs):
+
+    np.random.shuffle(allIndices)
+    
+    # put model in training mode
+    model.train()
+    
+    trainLoss  = 0
+    trainSteps = 0
+    
+    for indices in np.array_split(allIndices, minibatchSize):
+        
+        optimizer.zero_grad()
+    
+        # forward through the network
+        # inpuTrain[index] is each auction
+        # added features for each auction in this batch should be passed as input
+
+        output = model.forward([ inputTrain[index] for index in indices]) #, [added_featuresTrain[index] for index in indices])
+
+    
+        # build a PyTorch variable with the target value
+        # so that we can propagate backwards afterwards
+        thisTarget = Variable(
+            torch.from_numpy(np.stack([ targetTrain[index] for index in indices ])))
+    
+        # calculate loss
+        loss = lossFunc.forward(output, thisTarget)
+        # loss = lossFunc(output, thisTarget)
+        
+        # accumulate 
+        trainLoss += loss
+        
+        # backpropagate 
+        loss.backward()
+                            
+        # update learning rate        
+        optimizer.step()
+        
+        trainSteps += 1
+
+        
+    trainLoss /= trainSteps
+    trainLosses.append(trainLoss)    
+
+    # evaluate model on test set
+    model.eval()
+        
+    output = model.forward(inputTest)
+            
+    # calculate loss on test set
+    # testLoss = lossFunc.forward(output, testTargetVar).data  
+    testLoss = lossFunc(output,testTargetVar)
+    testLosses.append(testLoss)
+    
+    print("epoch",epoch,"train loss=", trainLoss.data, "test loss=",testLoss.data)
+
+
+
+model.eval()
+
+out = model.forward(inputTest).data.numpy() 
+# not sure how to interpret this output
+
+
+#plotting the loss
+trainLosses = [trainLosses[i].detach().numpy() for i,_ in enumerate(trainLosses)]
+testLosses = [testLosses[i].detach().numpy() for i,_ in enumerate(testLosses)]
+
+plt.plot(trainLosses)
+plt.plot(testLosses)
+plt.title('Loss vs Epochs')
+plt.xlabel('Epochs')
+plt.ylabel('loss')
+plt.legend(["train loss", "test loss"])
+plt.show()
+
+
+# true_pred = testTargetVar.numpy()
+# conf_matrix = confusion_matrix(true_pred,predictions)
+# print("Confusion Matrix of the Test Set")
+# print("-----------")
+# print(conf_matrix)
+# print("Accuracy :\t"+str(accuracy_score(true_pred,predictions)))
+# print("Precision :\t"+str(precision_score(true_pred,predictions)))
+# print("Recall :\t"+str(recall_score(true_pred,predictions)))
+# print("F1 Score :\t"+str(f1_score(true_pred,predictions)))
+    
+
---
+++ torch_model_G.py
@@ -0,0 +1,135 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+import operator
+
+import numpy as np
+np.random.seed(1773)
+
+import matplotlib.pyplot as plt
+import math
+
+
+
+class Net(nn.Module):
+    
+    def __init__(self, 
+                 numLayersInputSide  =  5, 
+                 widthInputSide      = 50, 
+                 numLayersOutputSide = 5, 
+                 widthOutputSide     = 50, 
+                ):
+        
+        super(Net, self).__init__()
+        
+        #----------
+        # input side layers
+        #----------
+            
+        # NETWORK 1
+        numInputs = 1
+        
+        self.inputSideLayers = []
+     
+        for i in range(numLayersInputSide): 
+            layer = nn.Linear(numInputs, widthInputSide, dtype=torch.float64)
+            self.inputSideLayers.append(layer)
+            self.add_module("iLayer%d" % i, layer)
+            
+            numInputs = widthInputSide 
+
+        #----------
+        # output side layers
+        #----------
+
+        # NETWORK 2
+        numNewfeatures = 1  # to add the new features as another input to the second model
+        numInputs = widthInputSide + numNewfeatures
+        numOutputs = widthOutputSide
+        
+        self.outputSideLayers = []
+        for i in range(numLayersOutputSide):
+        
+            if i == numLayersOutputSide - 1:
+                numOutputs = 1
+            else:
+                numOutputs = widthOutputSide
+            
+            layer = nn.Linear(numInputs, numOutputs, dtype=torch.float64)
+            self.outputSideLayers.append(layer)
+            self.add_module("oLayer%d" % i, layer)
+            
+            numInputs = numOutputs 
+             
+    #----------------------------------------
+    
+    def forward(self, points, added_features): # added_features are the variables like skewness, kurtosis, etc
+        # points is one batch, so a collection of auctions: each auction is an array of arrays (the internal ones are the bids)
+
+    
+        
+        # overall output for the entire minibatch
+        outputs = []
+        
+        # loop over minibatch entries
+        for indexAuction, thisPoints in enumerate(points):         # loop on the auctions
+
+
+            # outputs of each point of this minibatch entry
+            thisOutputs = [ ]
+            
+            # thisPoints is a list of 1D tensors (ogni bids è a sua volta un array)
+            # stack all input points into a 2D tensor
+            # (the second dimension will have size 1)
+            h = np.stack(thisPoints)
+            
+            h = Variable(torch.from_numpy(h)) # A PyTorch Variable is a wrapper around a PyTorch Tensor, 
+                                              # and represents a node in a computational graph. If x is a Variable 
+                                              # then x.data is a Tensor giving its value, and x.grad is another Variable 
+                                              # holding the gradient of x with respect to some scalar value.
+            
+            # forward all input points through the input side network
+            # each auction passes through each layer and relu
+            for layer in self.inputSideLayers:
+                h = layer(h)
+                h = F.relu(h)
+                            
+            # average the input side network outputs: sum along first dimension (point index), 
+            # then divide by number of points
+            # (it aggregates the output tensors of the first network).
+
+            output = h.sum(0) / len(thisPoints)
+            
+            # forward step of the second network
+            # here we should add added_features, an array of 26 elements for each auction
+
+            h = output
+            new_features = Variable(torch.from_numpy(np.asarray(added_features[indexAuction])))
+
+            for layerIndex, layer in enumerate(self.outputSideLayers):
+
+                # if layerIndex==0: # if we are in the first layer we have to consider the new features
+                #     h = layer(h,new_features)
+                # else:
+                    # h = layer(h)
+                h= layer(h)
+                
+                if layerIndex != len(self.outputSideLayers) - 1:
+                    h = torch.relu(h) 
+                # else: 
+                #     h = torch.sigmoid(h) # to have values between 0 and 1 (needed if the chosen loss function is BCEloss)
+                
+            outputs.append(h)
+            
+        # end of loop over minibatch entries
+         
+        # convert the list of outputs to a torch 2D tensor
+    
+        return torch.cat(outputs, 0)            
+
+
+
+
